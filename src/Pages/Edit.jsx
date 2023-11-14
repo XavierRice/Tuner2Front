8a2a83f174233b46d42477e4f6dd3fc3709bf04a
xavierrice <xavier.rice@gmail.com>
2023-11-13 23:29:58 -0500

    first commit

---
+++ src/Pages/Edit.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+function Edit() {
+    return (
+      <div className="New Edit">
+        <h2>Edit</h2>
+        <BookmarkEditForm />
+      </div>
+    );
+  }
+  
+  export default Edit;
